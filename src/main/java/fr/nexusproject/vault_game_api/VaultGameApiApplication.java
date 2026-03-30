cdadf2cf1e2faa3ee6b987506f8096e298394b95
Mathis Brebion <mathis.brebion@icloud.com>
2026-03-30 11:57:40 +0200

    First commit: initialize spring boot project

---
+++ src/main/java/fr/nexusproject/vault_game_api/VaultGameApiApplication.java
@@ -0,0 +1,13 @@
+package fr.nexusproject.vault_game_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultGameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultGameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/nexusproject/vault_game_api/VaultGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nexusproject.vault_game_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultGameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
